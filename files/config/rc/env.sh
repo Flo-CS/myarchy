f089d140bc7a9849dbc3b4db19fd66a2000e2dc3
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-11-11 22:05:58 +0100

    fix

--- files/config/rc/env.sh
+++
@@ -1,5 +0,0 @@
-export TERMINAL=alacritty
-export EDITOR=/usr/bin/nvim
-export SYSTEMD_EDITOR=/usr/bin/nvim
-
-export PATH="$HOME/.local/bin:$PATH"
--- files/config/rc/profile.sh
+++ files/config/rc/profile.sh
@@ -1,4 +1,6 @@
-# TODO: rename and split
+export TERMINAL=alacritty
+export EDITOR=/usr/bin/nvim
+export SYSTEMD_EDITOR=/usr/bin/nvim
 
 # Can also be set using hyprctl setcursor
 export HYPRCURSOR_THEME=BreezeX-RoséPineDawn
@@ -16,6 +18,8 @@ export __GLX_VENDOR_LIBRARY_NAME=nvidia
 
 source ~/.config/fzf/fzf.sh
 source ~/.config/gum/gum.sh
+
+export PATH="$HOME/.local/bin:$PATH"
 
 if uwsm check may-start; then
 	exec uwsm start hyprland.desktop
