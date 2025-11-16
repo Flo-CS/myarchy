46cc3cdfb7a3b1197086bcc8d9d80912f7237d6a
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-11-16 16:51:42 +0100

    add cursor

--- files/config/rc/profile.sh
+++ files/config/rc/profile.sh
@@ -2,13 +2,6 @@ export TERMINAL=alacritty
 export EDITOR=/usr/bin/nvim
 export SYSTEMD_EDITOR=/usr/bin/nvim
 
-# Can also be set using hyprctl setcursor
-export HYPRCURSOR_THEME=BreezeX-RoséPineDawn
-export HYPRCURSOR_SIZE=24
-
-export XCURSOR_THEME=BreezeX-RoséPineDawn
-export XCURSOR_SIZE=24
-
 export QT_QPA_PLATFORM='wayland;xcb'
 export QT_QPA_PLATFORMTHEME=qt6ct
 export QT_STYLE_OVERRIDE=Kvantum
