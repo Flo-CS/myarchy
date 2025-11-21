6b3e3fd3256ba1d5d58a9f54d1222278d4a8955a
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-11-21 20:56:25 +0100

    battery

--- files/config/hypr/default/hyprlock/status.sh
+++ files/config/hypr/default/hyprlock/status.sh
@@ -1,27 +1,14 @@
 #!/usr/bin/env bash
 
-############ Variables ############
-enable_battery=false
-battery_charging=false
+presence=$(myarchy-battery presence)
+state=$(myarchy-battery state)
 
-####### Check availability ########
-for battery in /sys/class/power_supply/*BAT*; do
-  if [[ -f "$battery/uevent" ]]; then
-    enable_battery=true
-    if [[ $(cat /sys/class/power_supply/*/status | head -1) == "Charging" ]]; then
-      battery_charging=true
-    fi
-    break
-  fi
-done
-
-############# Output #############
-if [[ $enable_battery == true ]]; then
-  if [[ $battery_charging == true ]]; then
+if [[ $presence == true ]]; then
+  if [[ $state == "charging" ]]; then
     echo -n "(+) "
   fi
-  echo -n "$(cat /sys/class/power_supply/*/capacity | head -1)"%
-  if [[ $battery_charging == false ]]; then
+  echo $(myarchy-battery percentage)"%"
+  if [[ $state == "discharging" ]]; then
     echo -n " remaining"
   fi
 fi
