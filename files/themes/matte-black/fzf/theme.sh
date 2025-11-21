b5bac612343a30e7a5d7a50a6d163d9e0f23c5d2
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-11-21 23:45:14 +0100

    fix fzf background

--- files/themes/matte-black/fzf/theme.sh
+++ files/themes/matte-black/fzf/theme.sh
@@ -1,4 +1,4 @@
-echo "--color=fg:#8a8a8d,bg:#121212,hl:#f59e0b
+echo "--color=fg:#8a8a8d,hl:#f59e0b
 --color=fg+:#eaeaea,bg+:#333333,hl+:#f59e0b
 --color=border:#8a8a8d,header:#f59e0b,gutter:#121212
 --color=spinner:#FFC107,info:#f59e0b
