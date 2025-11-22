4368880b86214a68e1740d269a974620bbfecdce
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-11-23 00:31:01 +0100

    stuff

--- files/themes/osaka-jade/fzf/theme.sh
+++
@@ -1,5 +0,0 @@
-echo="--color=fg:#C1C497,hl:#2DD5B7
---color=fg+:#F6F5DD,bg+:#23372B,hl+:#2DD5B7
---color=border:#53685B,header:#549e6a,gutter:#111c18
---color=spinner:#E5C736,info:#549e6a
---color=pointer:#2DD5B7,marker:#FF5345,prompt:#C1C497"
