bdba09b9d3ca3d2836a8fdc8b389d9d048e51d01
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-11-23 00:23:09 +0100

    replace matte-black by matte-ember

--- files/themes/matte-black/fzf/theme.sh
+++
@@ -1,5 +0,0 @@
-echo "--color=fg:#8a8a8d,hl:#f59e0b
---color=fg+:#eaeaea,bg+:#333333,hl+:#f59e0b
---color=border:#8a8a8d,header:#f59e0b,gutter:#121212
---color=spinner:#FFC107,info:#f59e0b
---color=pointer:#f59e0b,marker:#D35F5F,prompt:#8a8a8d"
