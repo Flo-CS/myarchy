623d94a6cd8bcc9081d3e92f7d7e6aae8db5d775
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-11-22 23:45:44 +0100

    stuff

---
+++ files/themes/forest-night/fzf/theme.sh
@@ -0,0 +1,5 @@
+echo "--color=fg:#56685d,hl:#4a9e6f
+--color=fg+:#e7efe8,bg+:#1e2d24,hl+:#4a9e6f
+--color=border:#56685d,header:#4a9e6f,gutter:#060906
+--color=spinner:#c2a44a,info:#4a9e6f
+--color=pointer:#4a9e6f,marker:#2f7a5b,prompt:#56685d"
