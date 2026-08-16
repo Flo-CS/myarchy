6d4e51d4577756231832996a30438d8840e0c299
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2026-08-16 22:17:04 +0200

    waybar idle-inhibitor

--- config/waybar/scripts/idle-inhibitor.sh
+++ config/waybar/scripts/idle-inhibitor.sh
@@ -3,13 +3,13 @@
 idle-inhibitor watch | while IFS= read -r line; do
 	case "$line" in
 	"inhibited")
-		printf '{"text": "☕", "tooltip": "Idle inhibited — click to toggle", "class": "active"}\n'
+		printf '{"text": "󰅶", "tooltip": "Idle inhibited — click to toggle", "class": "active"}\n'
 		;;
 	"not inhibited")
-		printf '{"text": "💤", "tooltip": "Idle not inhibited — click to toggle", "class": "inactive"}\n'
+		printf '{"text": "󰛊", "tooltip": "Idle not inhibited — click to toggle", "class": "inactive"}\n'
 		;;
 	esac
 done
 
-printf '{"text": "⚠", "tooltip": "idle-inhibitor daemon is not running", "class": "error"}\n'
+printf '{"text": "󰀦", "tooltip": "idle-inhibitor daemon is not running", "class": "error"}\n'
 sleep 5
